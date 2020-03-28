2f11f657864842cc90039986ca25c4e6bcc0836f
jithendaraa <jithen.subra@gmail.com>
2020-03-29 00:50:55 +0530

    add ActorNetwork
    - correct f1 to f2 in CriticNet

--- DDPG/Pendulum_torch/ddpg_model_pytorch.py
+++ DDPG/Pendulum_torch/ddpg_model_pytorch.py
@@ -57,7 +57,7 @@ class ReplayBuffer(object):
 class CriticNetwork(nn.Module):
     def __init__(self, beta, input_dims, fc1_dims, fc2_dims, n_actions, name,
                 chkpt_dir='tmp/ddpg'):
-        super(CriticNetwork, self).__init__()
+        super(ActorNetwork, self).__init__()
         self.input_dims = input_dims
         self.fc1_dims = fc1_dims
         self.fc2_dims = fc2_dims
@@ -73,7 +73,7 @@ class CriticNetwork(nn.Module):
         self.bn1 = nn.LayerNorm(self.fc1_dims)
 
         self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-        f1 = 1/np.sqrt(self.fc2.weight.data.size()[0])
+        f2 = 1/np.sqrt(self.fc2.weight.data.size()[0])
         torch.nn.init.uniform_(self.fc2.weight.data, -f2, f2)
         torch.nn.init.uniform_(self.fc2.bias.data, -f2, f2)
 
@@ -85,7 +85,6 @@ class CriticNetwork(nn.Module):
 
         self.optimizer = optim.Adam(self.parameters(), lr=beta)
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
         self.to(self.device)
 
     def forward(self, state, action):
@@ -101,6 +100,55 @@ class CriticNetwork(nn.Module):
 
         return state_action_value
 
+    def save_checkpoint(self):
+        print('...saving checkpoint...')
+        torch.save(self.state_dict(), self.checkpoint_file)
+    
+    def load_checkpoint(self):
+        print('...loading checkpoint...')
+        self.load_state_dict(torch.load(self.checkpoint_file))
+
+class ActorNetwork(nn.Module):
+    def __init__(self, alpha, input_dims, fc1_dims, fc2_dims, n_actions, name,
+                chkpt_dir='tmp/ddpg'):
+        super(CriticNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.checkpoint_file = os.path.join(chkpt_dir, name+'_ddpg')
+        
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        f1 = 1/np.sqrt(self.fc1.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc1.weight.data, -f1, f1)
+        torch.nn.init.uniform_(self.fc1.bias.data, -f1, f1)
+        self.bn1 = nn.LayerNorm(self.fc1_dims)
+        
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        f2 = 1/np.sqrt(self.fc2.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc2.weight.data, -f2, f2)
+        torch.nn.init.uniform_(self.fc2.bias.data, -f2, f2)
+        self.bn2 = nn.LayerNorm(self.fc2_dims)
+        
+        f3 = 0.003
+        self.mu = nn.Linear(self.fc2_dims, self.n_actions)
+        torch.nn.init.uniform_(self.mu.weight.data, -f3, f3)
+        torch.nn.init.uniform_(self.mu.bias.data, -f3, f3)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=beta)
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.to(self.device)
+    
+    def forward(self, state):
+        x = self.fc1(state)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        # Bound by +1 to -1 
+        x = torch.tanh(self.mu(x))
+    
     def save_checkpoint(self):
         print('...saving checkpoint...')
         torch.save(self.state_dict(), self.checkpoint_file)
