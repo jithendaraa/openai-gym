fc84208d462079adef5b65207531077f9b0659ad
jithendaraa <jithen.subra@gmail.com>
2020-03-29 18:19:07 +0530

    add Bipedal Agent class in ddpg_tf model

--- DDPG/BipedalWalkerCont_tf/ddpg_model_tf.py
+++ DDPG/BipedalWalkerCont_tf/ddpg_model_tf.py
@@ -119,7 +119,7 @@ class Actor(object):
         self.saver.save(self.sess, self.checkpoint_file)
     
 class Critic(object):
-    def __init__(self, lr, n_actions, name, input_dims, sess, fc1_dims, fc2_dims, action_bound, batch_size=64, chkpt_dir='tmp/ddpg'):
+    def __init__(self, lr, n_actions, name, input_dims, sess, fc1_dims, fc2_dims, batch_size=64, chkpt_dir='tmp/ddpg'):
         self.lr = lr
         self.n_actions = n_actions
         self.fc1_dims = fc1_dims
@@ -199,3 +199,101 @@ class Critic(object):
     def save_checkpoint(self):
         print('...saving checkpoint...')
         self.saver.save(self.sess, self.checkpoint_file)
+
+class Agent(object):
+    def __init__(self, alpha, beta, input_dims, tau, env, gamma=0.99,
+                n_actions=2, max_size=1000000, layer1_size=400, layer2_size=300,
+                batch_size=64, chkpt_dir='tmp/ddpg'):
+
+        self.gamma = gamma
+        self.tau = tau  
+        self.memory = ReplayBuffer(max_size, input_dims, n_actions)
+        self.batch_size = batch_size
+        self.sess = tf.Session()
+
+        self.actor = Actor(alpha, n_actions, 'Actor', 
+                        input_dims, self.sess, layer1_size, 
+                        layer2_size, env.action_space.high, 
+                        chkpt_dir=chkpt_dir)
+
+        self.critic = Critic(beta, n_actions, 'Critic', 
+                            input_dims, self.sess, layer1_size, 
+                            layer2_size, chkpt_dir=chkpt_dir)
+
+        self.target_actor = Actor(alpha, n_actions, 'TargetActor', 
+                        input_dims, self.sess, layer1_size, 
+                        layer2_size, env.action_space.high, 
+                        chkpt_dir=chkpt_dir)
+
+        self.target_critic = Critic(beta, n_actions, 'TargetCritic', 
+                            input_dims, self.sess, layer1_size, 
+                            layer2_size, chkpt_dir=chkpt_dir)
+        
+        self.noise = OUActionNoise(mu=np.zeros(n_actions))
+
+        # Update critic and actor params as in the DDPG paper
+        self.update_critic = [ self.target_critic.params[i].assign(
+            tf.multiply(self.critic.params[i], self.tau) + tf.multiply(self.target_critic.params[i], 1. - self.tau))
+            for i in range(len(self.target_critic.params)) ]
+        
+        self.update_actor = [ self.target_actor.params[i].assign(
+            tf.multiply(self.actor.params[i], self.tau) + tf.multiply(self.target_actor.params[i], 1. - self.tau))
+            for i in range(len(self.target_actor.params)) ]
+        
+        self.sess.run(tf.global_variables_initializer())
+        self.update_network_parameters(first=True)
+
+    def update_network_parameters(self, first=False):
+        if first:
+            old_tau = self.tau
+            self.tau = 1.0
+            self.target_critic.sess.run(self.update_critic)
+            self.target_actor.sess.run(self.update_actor)
+            self.tau = old_tau
+        else:
+            self.target_critic.sess.run(self.update_critic)
+            self.target_actor.sess.run(self.update_actor)
+    
+    def remember(self, state, actions, reward, state_, done):
+        self.memory.store_transition(state, actions, reward, state_, done)
+    
+    def choose_action(self, state):
+        # 1d to 2d np.ndarray
+        state = state[np.newaxis, :]
+        mu = self.actor.predict(state)
+        mu_prime = mu + self.noise()
+
+        return mu_prime[0]
+    
+    def learn(self):
+        if self.memory.mem_cntr < self.batch_size:
+            return 
+        
+        state, action, reward, new_state, done = self.memory.sample_buffer(self.batch_size)
+
+        critic_value_ = self.target_critic.predict(new_state, self.target.actor.predict(new_state))
+
+        target = []
+        for j in range(self.batch_size):
+            target.append(reward[j] + self.gamma * critic_value_[j] * done[j])
+        target = np.reshape(target, (self.batch_size, 1))
+
+        _ = self.critic.train(state, action, target)
+
+        a_outs = self.actor.predict(state)
+        grads = self.critic.get_action_gradients(state, a_outs)
+        self.actor.train(state, grads[0])
+
+        self.update_network_parameters()
+    
+    def save_models(self):
+        self.actor.save_checkpoints()
+        self.target_actor.save_checkpoints()
+        self.critic.save_checkpoints()
+        self.target_critic.save_checkpoints()
+    
+    def load_models(self):
+        self.actor.load_checkpoints()
+        self.target_actor.load_checkpoints()
+        self.critic.load_checkpoints()
+        self.target_critic.load_checkpoints()
