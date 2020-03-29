8a48ac962c2391c0a18a3bb2463610fd84e3b260
jithendaraa <jithen.subra@gmail.com>
2020-03-29 17:46:59 +0530

    add critic class

--- DDPG/BipedalWalkerCont_tf/ddpg_model_tf.py
+++ DDPG/BipedalWalkerCont_tf/ddpg_model_tf.py
@@ -118,6 +118,84 @@ class Actor(object):
         print('...saving checkpoint...')
         self.saver.save(self.sess, self.checkpoint_file)
     
+class Critic(object):
+    def __init__(self, lr, n_actions, name, input_dims, sess, fc1_dims, fc2_dims, action_bound, batch_size=64, chkpt_dir='tmp/ddpg'):
+        self.lr = lr
+        self.n_actions = n_actions
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.chkpt_dir = chkpt_dir
+        self.input_dims = input_dims
+        self.batch_size = batch_size
+        self.sess = sess
+        self.action_bound = action_bound
+        
+        self.build_network()
+        self.params = tf.trainable_variables(scope=self.name)
+        self.saver = tf.train.Saver()
+        self.checkpoint_file = os.path.join(chkpt_dir, name + '_ddpg.ckpt')
+        
+        self.optimize = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
 
+        self.action_gradients = tf.gradients(self.q, self.actions)
 
+    def build_network(self):
+        with tf.variable_scope(self.name):
+            self.input = tf.placeholder(tf.float32, shape=[None, *self.input_dims], name='inputs')
+            self.actions = tf.placeholder(tf.float32, shape=[None, *self.n_actions], name='actions')
+            self.q_target = tf.placeholder(tf.float32, shape=[None, 1], name='targets')
+
+            f1 = 1. / np.sqrt(self.fc1_dims)
+            dense1 = tf.layers.dense(self.input, units=self.fc1_dims,
+                                    kernel_initializer=random_uniform(-f1, f1),
+                                    bias_initializer=random_uniform(-f1, f1))
+            batch1 = tf.layers.batch_normalization(dense1)
+            layer1_activation = tf.nn.relu(batch1)
+
+            f2 = 1. / np.sqrt(self.fc2_dims)
+            dense2 = tf.layers.dense(self.input, units=self.fc2_dims,
+                                    kernel_initializer=random_uniform(-f2, f2),
+                                    bias_initializer=random_uniform(-f2, f2))
+            batch2 = tf.layers.batch_normalization(dense2)
+
+            action_in = tf.layers.dense(self.actions, units=self.fc2_dims, activation='relu')
+
+            state_actions = tf.add(batch2, action_in)
+            state_actions = tf.nn.relu(state_actions)
+
+            f3 = 0.003
+            self.q = tf.layers.dense(state_actions, units=1, 
+                                    kernel_initializer=random_uniform(-f3, f3)
+                                    bias_initializer=random_uniform(-f3, f3), 
+                                    kernel_regularizer=tf.keras.kernel_regularizers.l2(0.01))
             
+            self.loss = tf.loss.mean_squared_error(self.q_target, self.q)
+
+    def predict(self, inputs, actions):
+        return self.sess.run(self.q,
+                            feed_dict={
+                                self.input: inputs,
+                                self.actions: actions
+                            })
+        
+    def train(self, inputs, actions, q_target):
+        return self.sess.run(self.optimize, feed_dict={
+            self.input: inputs,
+            self.actions: actions,
+            self.q_target: q_target
+        })
+        
+    def get_action_gradients(self, inputs, actions):
+        return self.sess.run(self.action_gradients, 
+                            feed_dict={
+                                self.input: inputs,
+                                self.actions: actions
+                            })
+
+    def load_checkpoint(self):
+        print('...loading checkpoint...')
+        self.saver.restore(self.sess, self.checkpoint_file)
+    
+    def save_checkpoint(self):
+        print('...saving checkpoint...')
+        self.saver.save(self.sess, self.checkpoint_file)
