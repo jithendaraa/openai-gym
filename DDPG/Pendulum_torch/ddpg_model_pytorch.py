8760252d1990441be603a3a913c97ab4f400264b
jithendaraa <jithen.subra@gmail.com>
2020-03-29 11:22:33 +0530

    fix OUActionNoise formula in __call__

--- DDPG/Pendulum_torch/ddpg_model_pytorch.py
+++ DDPG/Pendulum_torch/ddpg_model_pytorch.py
@@ -15,7 +15,7 @@ class OUActionNoise(object):
         self.reset()
 
     def __call__(self):
-        x = self.x_prev + self.theta + (self.mu - self.x_prev) * self.dt + \
+        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
             self.sigma * np.sqrt(self.dt)*np.random.normal(size=self.mu.shape)
         self.x_prev = x
         return x 
